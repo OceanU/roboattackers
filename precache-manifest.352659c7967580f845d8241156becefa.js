0e8a71b0837b7e011014d5205202cf0eae569ff0
OceanU <oceanu.ionut@yahoo.ro>
2019-10-18 18:00:18 +0300

    Updates

---
+++ precache-manifest.352659c7967580f845d8241156becefa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66dad401eeaf7761c3e8d2c849603ba",
+    "url": "/roboattackers/index.html"
+  },
+  {
+    "revision": "8f86a58503b13961df4e",
+    "url": "/roboattackers/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6e5ee088b7a3fe774246",
+    "url": "/roboattackers/static/css/main.7a12566b.chunk.css"
+  },
+  {
+    "revision": "8f86a58503b13961df4e",
+    "url": "/roboattackers/static/js/2.cf4f0a0f.chunk.js"
+  },
+  {
+    "revision": "6e5ee088b7a3fe774246",
+    "url": "/roboattackers/static/js/main.ebb2ac39.chunk.js"
+  },
+  {
+    "revision": "23e342a6cca87b9c5b35",
+    "url": "/roboattackers/static/js/runtime-main.f18c879d.js"
+  },
+  {
+    "revision": "bcfa3a4184aa5b37b2a4033f6ec15325",
+    "url": "/roboattackers/static/media/Team 401.bcfa3a41.woff"
+  }
+]);
